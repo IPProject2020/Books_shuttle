fee3dcafd313a9b16413901cf26542b33a1983b2
Jainbhavik332 <73811833+Jainbhavik332@users.noreply.github.com>
2020-11-08 11:16:35 +0530

    added style and buybook after main close with script

--- home1.php
+++ home1.php
@@ -24,6 +24,35 @@ include "sellbook.php";
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
     <script src='index.js'></script>
+    <style>
+      * {
+        box-sizing: border-box;
+      }
+
+      /* Create two equal columns that floats next to each other */
+      .column {
+        float: left;
+        width: 20%;
+        padding: 10px;
+        height: 100vh; /* Should be removed. Only for demonstration */
+      }
+      .column2 {
+        float: left;
+        width: 80%;
+        padding: 10px;
+        height: 100vh; /* Should be removed. Only for demonstration */
+      }
+
+      /* Clear floats after the columns */
+      .row:after {
+        content: "";
+        display: table;
+        clear: both;
+      }
+     
+
+
+</style>
 </head>
 
 <body>
@@ -60,6 +89,80 @@ include "sellbook.php";
                 <button id="btn2" data-target="#Sellbooksmodal" data-toggle="modal">Sell Books</button>
             </div>
         </main>
+         <div class="row">
+      <div class="column" style="background-color:var(--main-white-color)"><br><br>
+        <p style="text-align: center; font-size: 20px;"><i>CATEGORY </i></p><br><br>
+       <a  href="#" style="text-decoration: none;" onclick="myFunction()"> 
+          <div class="w3-container">
+            <div
+              class="w3-card-4"
+              style="width: 100%; "
+            >
+              <div class="w3-container w3-center" >
+                <p style="color: black;">ENGINEERING</p>
+              </div>
+            </div>
+          </div></a>
+        <br><br>
+        <a href="#" style="text-decoration: none;" onclick="myFunction1()">
+          <div class="w3-container">
+            <div
+              class="w3-card-4"
+              style="width: 100%; "
+            >
+              <div class="w3-container w3-center">
+                <p style="color: black;">MEDICAL</p>
+              </div>
+            </div>
+          </div></a
+        >
+        <br><br>
+        <a href="#" style="text-decoration: none;" onclick="myFunction2()">
+          <div class="w3-container">
+            <div
+              class="w3-card-4"
+              style="width: 100%; "
+            >
+              <div class="w3-container w3-center" >
+                <p style="color: black;"> BCOM </p>
+              </div>
+            </div>
+          </div></a
+        >
+        <br><br>
+        <a href="#" style="text-decoration: none;" onclick="myFunction3()">
+          <div class="w3-container">
+            <div
+              class="w3-card-4"
+              style="width: 100%; height:30px; "
+            >
+              <div class="w3-container w3-center" >
+                <p style="color: black;">BSC</p>
+              </div>
+            </div>
+          </div></a
+        >
+      </div>
+      <div class="column2" style="background-color:white">
+       <iframe id="myFrame" height="100%" width="100%" style="border-style: none;" src="https://www.bitdegree.com">
+       
+       </iframe>
+      </div>
+    </div>
+    <script>
+        function myFunction() {
+          document.getElementById("myFrame").src = "https://wwf.org";
+        }
+        function myFunction1() {
+          document.getElementById("myFrame").src = "https://www.google.com";
+        }
+        function myFunction2() {
+          document.getElementById("myFrame").src = "https://www.bitdegree.com";
+        }
+        function myFunction3() {
+          document.getElementById("myFrame").src = "prac2partA.html";
+        }
+        </script>
         <footer class="page-footer">
             <small>© Copyright 2018. All rights reserved.</small>
             <ul>
